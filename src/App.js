d2b80a75da9dececfd4782774f1d4d67a0fd26ac
Mgphone <51868037+Mgphone@users.noreply.github.com>
2023-09-25 23:59:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">This is for react</header>
+    </div>
+  );
+}
+
+export default App;
