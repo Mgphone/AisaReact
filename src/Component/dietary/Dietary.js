66fc8e7b199f7434cb2f0b5c355057a3559337d8
Mgphone <51868037+Mgphone@users.noreply.github.com>
2023-10-01 10:23:49 +0100

    switch button

--- src/Component/dietary/Dietary.js
+++ src/Component/dietary/Dietary.js
@@ -1,15 +1,36 @@
 import React from "react";
 import "./Dietary.css";
+import Switch from "./Switch";
+import BootStrapSwitchButton from "bootstrap-switch-button-react";
+
 function Dietary({ handleVegan, isVegan }) {
   return (
     <div className="Dietary">
-      <label htmlFor="veganCheckbox" className="switch">
+      {/* <Form>
+        <Form.Check
+          type="switch"
+          checked={isVegan}
+          onChange={handleVegan}
+          label="Vegan"
+        />
+      </Form> */}
+      <div className="switch">
+        <BootStrapSwitchButton
+          checked={isVegan}
+          onChange={handleVegan}
+          onlabel="On"
+          size="m"
+        />
+        <label>Vegan</label>
+        <BootStrapSwitchButton />
+        <label>Vegetarian</label>
+      </div>
+      {/* <Switch isVegan={isVegan} handleVegan={handleVegan} /> */}
+
+      {/* <label htmlFor="veganCheckbox" className="switch">
         <input
           type="checkbox"
           className="toggle_vegan"
-          // onChange={(e) => {
-          //   handleVegan(e.target.checked);
-          // }}
           id="toggle_btn"
           onChange={handleVegan}
         />
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./Component/App";
 import Nav from "./Component/NavBar/Nav";
 import Background from "./Component/background/Background";
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 ReactDOM.render(
   <React.StrictMode>
     <Nav />
