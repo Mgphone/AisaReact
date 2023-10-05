a0f976127c4b25960c5c5b619f36bba49d16daa7
Mgphone <51868037+Mgphone@users.noreply.github.com>
2023-10-05 12:06:37 +0100

    change scroll using react-scroll

--- src/Component/categoryBar/CategoryBar.js
+++ src/Component/categoryBar/CategoryBar.js
@@ -1,49 +1,89 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState, useRef } from "react";
+// import "./CategoryBar.css";
+
+// function CategoryBar({ groupedMenu }) {
+//   const categoryBarRef = useRef([]);
+//   categoryBarRef.current = Object.keys(groupedMenu);
+
+//   const [activeSection, setActiveSection] = useState("");
+
+//   const scrollToElement = (id) => {
+//     const element = document.getElementById(id);
+//     if (element) {
+//       element.scrollIntoView({ behavior: "smooth" });
+//     }
+//     setActiveSection(id);
+//   };
+
+//   const handleScroll = () => {
+//     const categoryBar = categoryBarRef.current;
+
+//     for (const item of categoryBar) {
+//       const section = document.getElementById(item);
+//       if (section) {
+//         const rect = section.getBoundingClientRect();
+//         if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+//           setActiveSection((prevActiveSection) => {
+//             console.log("Active Section:", item);
+//             return item;
+//           });
+//           break;
+//         }
+//       }
+//     }
+//   };
+
+//   useEffect(() => {
+//     window.addEventListener("scroll", handleScroll);
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, []);
+
+//   return (
+//     <div>
+//       <div className="categories">
+//         <div className="button-container">
+//           {categoryBarRef.current.map((item) => (
+//             <button
+//               key={item}
+//               onClick={() => scrollToElement(item)}
+//               style={{
+//                 backgroundColor: activeSection === item ? "#e5007e" : "",
+//               }}
+//             >
+//               {item}
+//             </button>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default CategoryBar;
+import React from "react";
 import "./CategoryBar.css";
+import { Link, animateScroll as scroll } from "react-scroll";
 function CategoryBar({ groupedMenu }) {
-  const [activeSection, setActiveSection] = useState("");
   const categoryBar = Object.keys(groupedMenu);
-
-  const scrollToElement = (id) => {
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
-  const handleScroll = () => {
-    // Check which section is currently in view
-    for (const item of categoryBar) {
-      const section = document.getElementById(item);
-      if (section) {
-        const rect = section.getBoundingClientRect();
-        if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-          setActiveSection(item);
-          break;
-        }
-      }
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
   return (
     <div>
       <div className="categories">
         <div className="button-container">
           {categoryBar.map((item) => (
-            <button
-              key={item}
-              onClick={() => scrollToElement(item)}
-              style={{
-                backgroundColor: activeSection === item ? "#e5007e" : "",
-              }}
-            >
-              {item}
+            <button key={item}>
+              {/* {item} */}
+              <Link
+                activeClass="active"
+                to={item}
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                {item}
+              </Link>
             </button>
           ))}
         </div>
