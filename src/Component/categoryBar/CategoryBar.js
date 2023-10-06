24dd836b29107cede6278cfaa52c841a7fac10f4
Mgphone <51868037+Mgphone@users.noreply.github.com>
2023-10-06 23:39:43 +0100

    change buttons of div in categoryBar

--- src/Component/categoryBar/CategoryBar.js
+++ src/Component/categoryBar/CategoryBar.js
@@ -67,21 +67,30 @@ import "./CategoryBar.css";
 import { Link, animateScroll as scroll } from "react-scroll";
 function CategoryBar({ groupedMenu }) {
   const categoryBar = Object.keys(groupedMenu);
-
+  const [activeButton, setAciveButton] = useState("");
+  const handleButtonActive = (to) => {
+    setAciveButton(to);
+  };
   return (
     <div>
       <div className="categories">
         <div className="button-container">
           {categoryBar.map((item) => (
-            <button key={item}>
+            <button
+              key={item}
+              className={`button-default ${
+                item === activeButton ? "button-active" : ""
+              }`}
+            >
               {/* {item} */}
               <Link
                 activeClass="active"
                 to={item}
                 spy={true}
                 smooth={true}
-                offset={-70}
+                offset={-150}
                 duration={500}
+                onSetActive={handleButtonActive}
               >
                 {item}
               </Link>
